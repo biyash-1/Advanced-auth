0f2e2b8bb5bd3f1eca2c40ab0f0192fa7f80d29c
biyash-1 <sthaalish39@gmail.com>
2025-04-22 23:26:38 +0545

    routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express"
 const router = express.Router();
 
-import { signupController,loginController } from "../controller/authController.js";
+import { signupController,loginController, verifyEmail } from "../controller/authController.js";
 router.post('/signup',signupController)
 router.post('/login',loginController)
+router.post('/verify_email',verifyEmail)
 
 
 export default router
