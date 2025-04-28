e671359880db766adb5e99e67b517d556bc170c9
biyash-1 <sthaalish39@gmail.com>
2025-04-28 23:37:26 +0545

    mailtrap

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -1,50 +1,16 @@
-// import { MailtrapClient } from "mailtrap";
-// import dotenv from "dotenv"
-// dotenv.config();
-
-
-
-// const TOKEN = "9c42a27d31afa9ff91daffca1cd636a8"
-// console.log("token", TOKEN);
-
-
-// export const client = new MailtrapClient({
-//   token: TOKEN,
-// });
-
-// export const sender = {
-//   email: "hello@demomailtrap.com",
-//   name: "Mailtrap Test",
-// };
-// const recipients = [
-//   {
-//     email: "bsstha21@gmail.com",
-//   }
-// ];
-
-// client
-//   .send({
-//     from: sender,
-//     to: recipients,
-//     subject: "You are awesome!",
-//     text: "Congrats for sending test email with Mailtrap!",
-//     category: "Integration Test",
-//   })
-//   .then(console.log, console.error);
 
 import { MailtrapClient } from "mailtrap";
 import dotenv from "dotenv";
 dotenv.config();
-const TOKEN = "9c42a27d31afa9ff91daffca1cd636a8"
 
-// Configuration exports
+const TOKEN = "3b38da97619bf29e8b0b5df01a2ce0f2";
+
 export const client = new MailtrapClient({
-  token:TOKEN
+  token: TOKEN,
+  testInboxId: 3649222,
 });
 
 export const sender = {
-  email: "hello@demomailtrap.com",
+  email: "hello@example.com",
   name: "Mailtrap Test",
 };
-
-// Remove the test send code from this file
