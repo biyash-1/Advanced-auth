57684e429ae0885c1b12db16315310ba5bf77df9
biyash-1 <sthaalish39@gmail.com>
2025-04-30 23:55:47 +0545

    auth route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,13 +3,14 @@ const router = express.Router();
 import {verifyToken} from "../middleware/verifyToken.js"
 
 
-import { signupController,loginController, verifyEmail,forgetPassword ,resetPassword, checkAuth} from "../controller/authController.js";
+import { signupController,loginController, verifyEmail,forgetPassword ,resetPassword, checkAuth, logout} from "../controller/authController.js";
 router.post('/signup',signupController)
 router.post('/login',loginController)
 router.post('/verify_email',verifyEmail)
 router.post('/forgot_password',forgetPassword)
 router.get('/checkAuth',verifyToken,checkAuth)
 router.post('/reset_password/:token',resetPassword)
+router.post('/logout',logout)
 
 
 export default router;
